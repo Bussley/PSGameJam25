2eb6b435e12bbecf58c49fed44cfaa6fd9f7e50e
Bussley <snowboarderdude54@gmail.com>
2025-01-17 20:58:09 -0800

    Reworked redundant cursor logic, instead is selection.

--- PSGameJam/Assets/Scripts/CursorLogic.cs
+++ PSGameJam/Assets/Scripts/CursorLogic.cs
@@ -3,21 +3,12 @@ using UnityEngine.InputSystem;
 
 public class CursorLogic : MonoBehaviour
 {
-    [SerializeField]
-    private float mouse_x_offset = 0.0f;
-    [SerializeField]
-    private float mouse_y_offset = 0.0f;
-
-    [SerializeField]
-    private InputActionReference cursor_input;
-
-    private void Start() {
-        Cursor.visible = false;
+    public void MouseSelection(InputAction.CallbackContext context) {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, float.MaxValue))
+        {
+            //Logic on what is hit
+        }
     }
-
-    private void Update() {
-        // Get screen mouse position and set object to that world position
-        var cursor_pos = Camera.main.ScreenToWorldPoint(cursor_input.action.ReadValue<Vector2>());
-        transform.position = new Vector2(cursor_pos.x + mouse_x_offset, cursor_pos.y + mouse_y_offset);
-    }
-}
+ }
