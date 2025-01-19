b3136cedca8f945f289d4c595b8b6a66e88c442b
Bussley <snowboarderdude54@gmail.com>
2025-01-18 19:05:18 -0800

    Player Direction Implemented

--- PSGameJam/Assets/Scripts/PlayerController.cs
+++ PSGameJam/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
+    private enum Direction
+    {
+        N, NE, E, SE, S, SW, W, NW,
+        None
+    };
+
     [SerializeField]
     private Rigidbody2D rig;
 
@@ -16,6 +22,8 @@ public class PlayerController : MonoBehaviour
     private InputActionReference playerInput;
 
     private Vector2 moveDirection;
+    private Direction playerDirection;
+    private bool canMove;
 
     //Temp, get rid after testing or move it to better location
     [SerializeField]
@@ -23,17 +31,68 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject wheatTile;
 
-    private void Start() {
 
+    private void Start() {
+        playerDirection = Direction.S;
+        canMove = true;
     }
 
     private void Update() {
-        moveDirection = playerInput.action.ReadValue<Vector2>();
+
     }
 
     private void FixedUpdate() {
         rig.linearVelocity = new Vector2(moveDirection.x * moveSpeed,
                                          moveDirection.y * moveSpeed);
+    }
+
+    public void DetermineDirection(InputAction.CallbackContext context) {
+        // Get direction of player and move
+        if (context.performed && canMove)
+        {
+            moveDirection = playerInput.action.ReadValue<Vector2>();
+            int dir = (int)(Vector2.Angle(Vector2.up, moveDirection) / 45.0f);
+
+            if (dir == 0)
+                playerDirection = Direction.N;
+            else if (dir == 4)
+                playerDirection = Direction.S;
+            else if (moveDirection.x < 0)
+            {
+                if (dir == 1)
+                    playerDirection = Direction.NW;
+                else if (dir == 2)
+                    playerDirection = Direction.W;
+                else if (dir == 3)
+                    playerDirection = Direction.SW;
+            }
+            else if (moveDirection.x > 0)
+            {
+                if (dir == 1)
+                    playerDirection = Direction.NE;
+                else if (dir == 2)
+                    playerDirection = Direction.E;
+                else if (dir == 3)
+                    playerDirection = Direction.SE;
+            }
+            Debug.Log(playerDirection);
+        }
+        else if(context.canceled)
+            moveDirection = Vector2.zero;
+    }
+
+    public void FireLaser(InputAction.CallbackContext context) {
+        if(context.started) {
+            // Stop Player
+            moveDirection = Vector2.zero;
+            canMove = false;
+
+            // Spawn aimer
+
+        }
+        else if (context.canceled) {
+            canMove = true;
+        }
     }
 
     public void TestPlaceWhate(InputAction.CallbackContext context) {
@@ -42,7 +101,6 @@ public class PlayerController : MonoBehaviour
         {
             Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector3Int grid_pos = map.WorldToCell(mouse_pos);
-            //Debug.Log("Position at" + grid_pos);
 
             GameObject gameObjectAtPosition = map.GetInstantiatedObject(grid_pos);
             if (gameObjectAtPosition == null)
@@ -50,9 +108,6 @@ public class PlayerController : MonoBehaviour
                 map.SetTile(grid_pos, new Tile() { gameObject = wheatTile });
                 Debug.Log("test");
             }
-            //map.SetTile(grid_pos, new Tile() { gameObject = wheatTile });
-
-
         }
     }
 }
