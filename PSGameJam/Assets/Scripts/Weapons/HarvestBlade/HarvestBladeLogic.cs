80cac28294f805ac77b382138751fa708f6d78b8
Bussley <snowboarderdude54@gmail.com>
2025-01-20 15:27:24 -0800

    bunch of stuff. Read Description
    - Added Harvest Blade
    - Added Crop Prefab
    - Reorganized

---
+++ PSGameJam/Assets/Scripts/Weapons/HarvestBlade/HarvestBladeLogic.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class HarvestBladeLogic : MonoBehaviour
+{
+    [SerializeField] 
+    private float arcLength;
+
+    [SerializeField]
+    private float rotateAmount;
+
+    [SerializeField]
+    private Vector2 rotatePoint;
+
+    private float duration;
+    private float curTime;
+    private Vector3 offsetPosition;
+
+    private void Awake() {
+        curTime = 0;
+        offsetPosition = transform.parent.position + new Vector3(rotatePoint.x, rotatePoint.y, 0);
+    }
+
+    private void Update() {
+
+        if (curTime < duration)
+            transform.RotateAround(offsetPosition, Vector3.forward, -rotateAmount);
+        else
+        {
+            transform.parent.GetComponent<PlayerController>().SetMove(true);
+            Destroy(gameObject);
+        }
+
+        curTime++;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // HIT A CROP TO HARVEST. LOGIC TO CALL CROP.HARVEST HERE
+        Debug.Log("HIT");
+    }
+
+    public void Fire(int direction) {
+        // Find point infront of mech
+        float startAngle = 45 * (6 - direction);
+        if (direction > 4)
+            startAngle = 45 * (direction - 6);
+
+        // This is to get the correct X for hand swipe with sword
+        Vector3 xRot = TileManager.rotate(new Vector2(-rotatePoint.x, 0), startAngle);
+
+        startAngle += arcLength;
+        duration = (arcLength * 2) / rotateAmount;
+
+        // Set position to offset Position
+        offsetPosition += xRot;
+        transform.position = offsetPosition - new Vector3(transform.lossyScale.x/2, 0);
+
+        transform.RotateAround(offsetPosition, Vector3.forward, startAngle);
+    }
+}
