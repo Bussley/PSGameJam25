3572cb0475821da2563410ae1b5655abd80a7afe
Bussley <snowboarderdude54@gmail.com>
2025-01-21 16:47:17 -0800

    Duck logic and reorginization

---
+++ PSGameJam/Assets/Scripts/Duck/DuckLogic.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class DuckLogic : MonoBehaviour
+{
+    [SerializeField]
+    private float minTime;
+    [SerializeField] 
+    private float maxTime;
+    [SerializeField]
+    private Vector3 flySpot;
+    [SerializeField]
+    private Vector3 originSpot;
+    [SerializeField]
+    private float flyInSpeed;
+
+    private float flyInTime;
+    private Vector3 flyDirection;
+    private bool onSpot;
+    private bool flyOut;
+    private Rigidbody2D rig;
+
+
+    private void Awake() {
+        onSpot = false;
+        flyOut = false;
+        flyInTime = Time.time + Random.Range(minTime, maxTime);
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!onSpot && flyInTime < Time.time) {
+            FlyIn();
+        }
+        else if(flyOut) {
+            FlyOut();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        flyOut = true;
+    }
+    private void FlyIn()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, flySpot, flyInSpeed * Time.deltaTime);
+
+        if(transform.position == flySpot)
+            onSpot = true;
+    }
+
+    private void FlyOut()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, originSpot, flyInSpeed * Time.deltaTime);
+
+        if (transform.position == originSpot)
+        {
+            flyInTime = Time.time + Random.Range(minTime, maxTime);
+            onSpot = false;
+            flyOut = false;
+        }
+    }
+}
