5a796c708dec7f4ac6b97ca3928f26088ba59d6f
Bussley <snowboarderdude54@gmail.com>
2025-01-30 21:35:01 -0800

    Win screen in

--- PSGameJam/Assets/Scripts/WinScreen.cs
+++ PSGameJam/Assets/Scripts/WinScreen.cs
@@ -1,21 +1,26 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
     private GameObject winScreen;
+    private GameObject winText;
     private bool haswon;
 
     private void Awake()
     {
         winScreen = GameObject.FindGameObjectWithTag("WinScreen");
+        winText = GameObject.FindGameObjectWithTag("WinText");
         winScreen.SetActive(false);
         haswon = false;
     }
 
-    public void CloseWinScreen(InputAction.CallbackContext callbackContext)
+    private void Update()
     {
-        if(callbackContext.performed && haswon)
+        if(haswon && Input.GetKeyDown(KeyCode.F))
         {
             winScreen.SetActive(false);
         }
@@ -25,8 +30,31 @@ public class WinScreen : MonoBehaviour
     {
         if (!haswon)
         {
-            haswon = true;
             winScreen.SetActive(true);
+            StartCoroutine(FadeImageToFullAlpha(1.0f));
+            StartCoroutine(FadeTextToFullAlpha(30.0f));
+            haswon = true;
+        }
+    }
+
+    public IEnumerator FadeImageToFullAlpha(float t)
+    {
+        Image i = winScreen.GetComponent<Image>();
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+    public IEnumerator FadeTextToFullAlpha(float t)
+    {
+        var i = winText.GetComponent<TextMeshProUGUI>();
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
         }
     }
 }
